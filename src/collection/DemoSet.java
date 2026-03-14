ce062915a4ee0d20262144c1c9f9d62d67492eb3
arun-dh <arun.s@dedalus.eu>
2026-03-14 17:07:23 +0530

    Treeset

--- src/collection/DemoSet.java
+++ src/collection/DemoSet.java
@@ -44,6 +44,65 @@ public class DemoSet {
 
         System.out.println(new LinkedHashSet(users));
 
+
+        Set<Integer> set1 = new HashSet<>();
+        set1.add(1);
+        set1.add(2);
+        set1.add(3);
+
+        Set<Integer> set2 = new HashSet<>();
+        set2.add(11);
+        set2.add(2);
+        set2.add(33);
+
+        Set<Integer> union = new HashSet<>(set1);
+        union.addAll(set2);
+        System.out.println("Union: "+union);
+
+        Set<Integer> intersection = new HashSet<>(set1);
+        intersection.retainAll(set2);
+        System.out.println("intersection: "+intersection);
+
+        Set<Integer> difference = new HashSet<>(set1);
+        difference.removeAll(set2); //
+        System.out.println("difference: "+difference);
+
+        TreeSet<Integer> integerTreeSet = new TreeSet<>();
+        integerTreeSet.add(5);
+        integerTreeSet.add(3);
+        integerTreeSet.add(4);
+        integerTreeSet.add(6);
+        integerTreeSet.add(6);
+        integerTreeSet.add(0);
+
+        System.out.println(integerTreeSet);
+
+        TreeSet<String> stringTreeSet = new TreeSet<>();
+        stringTreeSet.add("raja");
+        stringTreeSet.add("raju");
+        stringTreeSet.add("nazrin");
+        stringTreeSet.add("Roja");
+
+        System.out.println(stringTreeSet);
+
+        TreeSet<Character> chars = new TreeSet<>();
+        String name = "raja";
+        for (int i = 0; i < name.length(); i++) {
+            chars.add(name.charAt(i));
+        }
+        System.out.println(chars);
+
+        System.out.println("subset(): "+stringTreeSet.subSet("Roja", "raja"));
+        System.out.println("subset(): "+stringTreeSet.subSet("Roja", false, "raja", true));
+
+        System.out.println("tail(): "+stringTreeSet.tailSet("nazrin"));
+        System.out.println("tail(): "+stringTreeSet.tailSet("nazrin", false));
+
+        TreeSet<User> userTreeSet = new TreeSet<>();
+        userTreeSet.add(new User("name", 30, 14));
+        userTreeSet.add(new User("raja", 60, 14));
+
+        System.out.println(userTreeSet);
     }
 
 }
